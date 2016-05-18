c2dc92cb506b319d120390375c07fdc05c4bffcb
uoleksii <tomin.o@dbbest.com>
2016-05-18 18:41:20 +0300

    Test commit

--- src/main/java/pack/Main.java
+++ src/main/java/pack/Main.java
@@ -4,6 +4,9 @@ package pack;
  * Created by Mezentsev.Y on 5/18/2016.
  */
 public class Main {
+    static {
+        System.out.println("Hello world!)))))");
+    }
     public static void main(String[] args) {
 
     }
