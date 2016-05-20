edce07e892a8fb65e57d2435507ca17cbea6b46d
yurchello <protect_info@list.ru>
2016-05-21 02:39:44 +0300

    Added dao functionality

--- src/main/java/com/mezentsev_tomin/adminpanel/dao/impl/UserDaoImpl.java
+++ src/main/java/com/mezentsev_tomin/adminpanel/dao/impl/UserDaoImpl.java
@@ -2,8 +2,14 @@ package com.mezentsev_tomin.adminpanel.dao.impl;
 
 import com.mezentsev_tomin.adminpanel.beans.User;
 import com.mezentsev_tomin.adminpanel.dao.UserDao;
+import com.mezentsev_tomin.adminpanel.util.HibernateUtil;
+import org.hibernate.Session;
 
+import javax.swing.*;
+import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by Mezentsev.Y on 5/19/2016.
@@ -11,31 +17,117 @@ import java.util.Collection;
 
 
 public class UserDaoImpl implements UserDao {
-    public void create(User user) {
 
+    public void create(User user) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (session != null && session.isOpen()){
+                session.close();
+            }
+        }
     }
 
     public void update(User user) {
-
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.update(user);
+            session.getTransaction().commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if (session != null && session.isOpen()){
+                session.close();
+            }
+        }
     }
 
     public void delete(Integer id) {
-
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            session.delete((User)session.load(User.class, id));
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
     }
 
     public User findById(Integer id) {
-        return null;
+        Session session = null;
+        User user = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            user = (User) session.get(User.class, id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+        return user;
     }
 
     public User findByEmail(String email) {
-        return null;
+        Session session = null;
+        User user = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            user = (User) session.get(User.class, email);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+        return user;
     }
 
     public User findByLogin(String login) {
-        return null;
+        Session session = null;
+        User user = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            user = (User) session.get(User.class, login);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+        return user;
     }
 
     public Collection<User> findAll() {
-        return null;
+        Session session = null;
+        List<User> users = new ArrayList<>();
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            users = session.createCriteria(User.class).list();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null && session.isOpen()) {
+                session.close();
+            }
+        }
+        return users;
     }
 }
