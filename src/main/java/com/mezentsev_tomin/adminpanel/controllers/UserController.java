799ca7630413279105309bf42da3ffc1a38aa11e
yurchello <protect_info@list.ru>
2016-06-07 23:40:13 +0300

    Added login form mapping. test example

--- src/main/java/com/mezentsev_tomin/adminpanel/controllers/UserController.java
+++ src/main/java/com/mezentsev_tomin/adminpanel/controllers/UserController.java
@@ -1,14 +1,21 @@
 package com.mezentsev_tomin.adminpanel.controllers;
 
+import com.mezentsev_tomin.adminpanel.beans.User;
+import com.mezentsev_tomin.adminpanel.exceptions.InvalidUserInputException;
 import com.mezentsev_tomin.adminpanel.forms.LoginForm;
 import com.mezentsev_tomin.adminpanel.servise.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by Yuriy on 05.06.2016.
@@ -18,30 +25,71 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/hello", method =  RequestMethod.GET)
+//    @RequestMapping(value = "/hello", method =  RequestMethod.GET)
+//    public String helloWorld(ModelMap modelMap){
+//          modelMap.addAttribute("message", "HELLO!!!");
+//          return "hello";
+//    }
+
+
+//    @RequestMapping(value = "/")
+//    public String home() {
+//        System.out.println();
+//        return "redirect:/login";
+//    }
+
+//    @RequestMapping(value ="/login", method=RequestMethod.GET)
+//    public String showLogin(Model model){
+//       model.addAttribute("loginForm",new LoginForm());
+//        return "login";
+//    }
+
+//    @RequestMapping(value = "/login", method = RequestMethod.POST)
+//    public ModelAndView login(	@RequestParam(value = "error", required = false) String error,
+//                                  @RequestParam(value = "logout", required = false) String logout){
+//        ModelAndView model = new ModelAndView();
+//        if (error != null) {
+//            model.addObject("error", "Invalid username and password!");
+//        }
+//
+//        if (logout != null) {
+//            model.addObject("msg", "You've been logged out successfully.");
+//        }
+//        model.setViewName("login");
+//
+//        return model;
+//    }
+
+    @RequestMapping(value = "/login", method =  RequestMethod.POST)
     public String helloWorld(ModelMap modelMap){
           modelMap.addAttribute("message", "HELLO!!!");
           return "hello";
     }
 
+//    @RequestMapping(value="/login", method=RequestMethod.POST)
+//    public String login(@ModelAttribute("loginForm") LoginForm loginForm,
+//                        Model model, HttpSession session){
+//
+//        try {
+//            User user = new User(666666L,
+//                    "User666",
+//                    "user666",
+//                    "666@.gmail.com",
+//                    "1111",
+//                    "/photo/p.jpg",
+//                    "someinfo" );
+//            session.setAttribute("CURRENT_ACCOUNT", user);
+//            return "login_ok";
+//        } catch (RuntimeException e) {
+//            return "login_not_ok";
+//        }
+//    }
 
-    @RequestMapping(value = "/")
-    public String home() {
-        System.out.println();
-        return "redirect:/login";
-    }
 
-    @RequestMapping(value ="/login", method=RequestMethod.GET)
+    @RequestMapping(value="/login", method=RequestMethod.GET)
     public String showLogin(Model model){
-       model.addAttribute("loginForm",new LoginForm());
+        model.addAttribute("loginForm", new LoginForm());
         return "login";
     }
-
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(@ModelAttribute("loginForm") LoginForm loginForm, Model model){
-        System.out.println();
-        return "hello";
-    }
-
 
 }
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -1,38 +1,69 @@
-<%@page contentType="text/html" pageEncoding="UTF-8"%>
-<!DOCTYPE html>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@page session="true"%>
 <html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>JSP Example</title>
+    <title>Login Page</title>
 </head>
-<body>
-<form method="post" action="login.jsp" commandName="loginForm">
-    <center>
-        <table border="1" width="30%" cellpadding="3">
-            <thead>
+<body onload='document.loginForm.username.focus();'>
+
+<h1>Login Form Name</h1>
+
+<div id="login-box">
+
+    <h2>Login with Username and Password</h2>
+
+    <c:if test="${not empty error}">
+        <div class="error">${error}</div>
+    </c:if>
+    <c:if test="${not empty msg}">
+        <div class="msg">${msg}</div>
+    </c:if>
+
+    <form name='loginForm' action="/login" method='POST'>
+
+        <table>
             <tr>
-                <th colspan="2">Login Here</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <td>User Name</td>
-                <td><input type="text" name="uname" value="" /></td>
+                <td>User:</td>
+                <td><input type='text' name='username'></td>
             </tr>
             <tr>
-                <td>Password</td>
-                <td><input type="password" name="pass" value="" /></td>
+                <td>Password:</td>
+                <td><input type='password' name='password' /></td>
             </tr>
             <tr>
-                <td><input type="submit" value="Login" /></td>
-                <td><input type="reset" value="Reset" /></td>
+                <td colspan='2'><input name="submit"  type="submit" value="submit" /></td>
             </tr>
-            <tr>
-                <td colspan="2">Yet Not Registered!! <a href="reg.jsp">Register Here</a></td>
-            </tr>
-            </tbody>
         </table>
-    </center>
-</form>
+
+        <%--<input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}" />--%>
+
+    </form>
+</div>
+
 </body>
 </html>
+<%--<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>--%>
+<%--<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>--%>
+<%--<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>--%>
+<%--<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>--%>
+<%--<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>--%>
+<%--<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>--%>
+
+<%--<div>--%>
+    <%--<h2>Hello form!</h2>--%>
+    <%--<form:form method="POST" action="${context }/login" commandName="loginForm" >--%>
+        <%--<table>--%>
+            <%--<tr>--%>
+                <%--<td colspan="2" class="errors"><form:errors path="*"/></td>--%>
+            <%--</tr>--%>
+            <%--<tr>--%>
+                <%--<td><form:label path="login">Login</form:label></td>--%>
+                <%--<td><form:input path="login" /></td>--%>
+            <%--</tr>--%>
+            <%--<tr>--%>
+                <%--<td><form:label path="password">Password</form:label></td>--%>
+                <%--<td><form:password path="password"/> </td>--%>
+            <%--</tr>--%>
+        <%--</table>--%>
+    <%--</form:form>--%>
+<%--</div>--%>
