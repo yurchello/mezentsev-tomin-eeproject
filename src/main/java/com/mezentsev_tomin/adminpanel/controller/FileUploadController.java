e680549baa896ef0638c8a975655373fa2194543
mezentsev.y <mezentsev.y@dbbest.com>
2016-09-09 13:14:53 +0300

    added vocabulary foreign key based  mapping.Refactoring

--- src/main/java/com/mezentsev_tomin/adminpanel/controller/FileUploadController.java
+++ src/main/java/com/mezentsev_tomin/adminpanel/controller/FileUploadController.java
@@ -27,49 +27,49 @@ import java.util.List;
 //@Controller
 public class FileUploadController {
 
-    private static String UPLOAD_LOCATION="C:/aaa/";
-
-    @Autowired
-    FileValidator fileValidator;
-
-    @Autowired
-    MultiFileValidator multiFileValidator;
-
-    @InitBinder("fileBucket")
-    protected void initBinderFileBucket(WebDataBinder binder) {
-        binder.setValidator(fileValidator);
-    }
-
-    @InitBinder("multiFileBucket")
-    protected void initBinderMultiFileBucket(WebDataBinder binder) {
-        binder.setValidator(multiFileValidator);
-    }
-
-
-    @RequestMapping(value = "/singleUpload", method = RequestMethod.GET)
-    public String getSingleUploadPage(ModelMap model) {
-        FileBucket fileModel = new FileBucket();
-        model.addAttribute("fileBucket", fileModel);
-        return "singleFileUploader";
-    }
-
-    @RequestMapping(value = "/singleUpload", method = RequestMethod.POST)
-    public String singleFileUpload(@Valid FileBucket fileBucket,
-                                   BindingResult result, ModelMap model) throws IOException {
-
-        if (result.hasErrors()) {
-            System.out.println("validation errors");
-            return "singleFileUploader";
-        } else {
-            System.out.println("Fetching file");
-            MultipartFile multipartFile = fileBucket.getFile();
-
-            // Now do something with file...
-            FileCopyUtils.copy(fileBucket.getFile().getBytes(), new File( UPLOAD_LOCATION + fileBucket.getFile().getOriginalFilename()));
-            String fileName = multipartFile.getOriginalFilename();
-            model.addAttribute("fileName", fileName);
-            return "success";
-        }
-    }
+//    private static String UPLOAD_LOCATION="C:/aaa/";
+//
+//    @Autowired
+//    FileValidator fileValidator;
+//
+//    @Autowired
+//    MultiFileValidator multiFileValidator;
+//
+//    @InitBinder("fileBucket")
+//    protected void initBinderFileBucket(WebDataBinder binder) {
+//        binder.setValidator(fileValidator);
+//    }
+//
+//    @InitBinder("multiFileBucket")
+//    protected void initBinderMultiFileBucket(WebDataBinder binder) {
+//        binder.setValidator(multiFileValidator);
+//    }
+//
+//
+//    @RequestMapping(value = "/singleUpload", method = RequestMethod.GET)
+//    public String getSingleUploadPage(ModelMap model) {
+//        FileBucket fileModel = new FileBucket();
+//        model.addAttribute("fileBucket", fileModel);
+//        return "singleFileUploader";
+//    }
+//
+//    @RequestMapping(value = "/singleUpload", method = RequestMethod.POST)
+//    public String singleFileUpload(@Valid FileBucket fileBucket,
+//                                   BindingResult result, ModelMap model) throws IOException {
+//
+//        if (result.hasErrors()) {
+//            System.out.println("validation errors");
+//            return "singleFileUploader";
+//        } else {
+//            System.out.println("Fetching file");
+//            MultipartFile multipartFile = fileBucket.getFile();
+//
+//            // Now do something with file...
+//            FileCopyUtils.copy(fileBucket.getFile().getBytes(), new File( UPLOAD_LOCATION + fileBucket.getFile().getOriginalFilename()));
+//            String fileName = multipartFile.getOriginalFilename();
+//            model.addAttribute("fileName", fileName);
+//            return "success";
+//        }
+//    }
 
 }
