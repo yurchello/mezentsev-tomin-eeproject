1fffe47c9eff28b8802ba4715c2ac3aae26f5748
mezentsev.y <mezentsev.y@dbbest.com>
2016-10-02 16:43:46 +0300

    wordGroup test connection

--- tmed-server/src/main/java/com/airplaneSoft/translateMeDude/api/wordGroupApi/WordGroupApi.java
+++ tmed-server/src/main/java/com/airplaneSoft/translateMeDude/api/wordGroupApi/WordGroupApi.java
@@ -29,12 +29,21 @@ public class WordGroupApi {
     //{"id":46,"ssoId":null,"password":"$2a$10$SJ2PcT/U4LvpypN76WYfkemo1C39tEf77W5IQpD9LQOhGvJ8l0PYG","firstName":null,"lastName":null,"email":null,"description":null,"photo":null,"userProfiles":[]}
     @Transactional
     @RequestMapping(value="/getGroupsList", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<WordsGroup>> test37(@RequestBody User user){
+    public ResponseEntity<List<WordsGroup>> getGroupsList(@RequestBody User user){
         User ownerUser = userService.findById(user.getId());
-        if (!Utils.getPasswordEncoder().matches(user.getPassword(), ownerUser.getPassword()))return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        if (!Utils.getPasswordEncoder().matches(user.getPassword(), ownerUser.getPassword()))
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         List<WordsGroup> list = wordGroupService.findAllUserGroups(ownerUser);
         if (list == null)return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/testConnection", method = RequestMethod.POST)
+    ResponseEntity<Void> testConnection(@RequestBody User user){
+        User ownerUser = userService.findById(user.getId());
+        if (!Utils.getPasswordEncoder().matches(user.getPassword(), ownerUser.getPassword()))
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        else return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
