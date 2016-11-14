7ac9e76bb7d4efbd12df7c0c0e01840f3ff0e4c8
mezentsev.y <mezentsev.y@dbbest.com>
2016-11-14 16:24:50 +0200

    refactoring

--- 2.bat
+++
@@ -1,1 +0,0 @@
-javapackager.exe -createjar -nocss2bin -appclass com.airplaneSoft.translateMeDude.winApp.App -srcdir tmed-win-desctop-ui/target/classes -outdir tmed-win-desctop-ui/target/packager_jar -outfile AWSSchemaConversionToolBatch.jar -v -manifestAttrs "Specification-Version=1.0,Implementation-Version=%BUILD_NUMBER%,Specification-Title=AWS Schema Conversion Tool"
